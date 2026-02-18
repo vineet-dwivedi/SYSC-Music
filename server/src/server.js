4e8ce98ee89801afd6fa9cdf9d681e512af58d3e
Vineet Dwivedi <vineetdwivedi409@gmail.com>
2026-02-18 21:22:40 +0530

    Updated server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,5 @@
 import app from './app.js'
-const PORT = 5000;
+const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
